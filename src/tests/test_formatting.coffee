9bf97756f8667c39f921ab74b25b5f4dd786cd20
Branko Vukelic <branko@brankovukelic.com>
2013-09-16 22:54:54 +0200

    Use the main method name in tests

--- src/tests/test_formatting.coffee
+++ src/tests/test_formatting.coffee
@@ -1,4 +1,4 @@
-# strftime formatting tests
+# toFormat formatting tests
 
 if require?
   chai = require 'chai'
@@ -10,92 +10,92 @@ equal = assert.equal
 d = new TzTime(2013, 8, 1, 18, 7, 8, 200)
 d1 = new TzTime(2013, 8, 1, 8, 7, 8, 200)
 
-describe 'datetime.format', () ->
-  describe 'strftime()', () ->
+describe 'TzTime', () ->
+  describe 'tpFormat()', () ->
     it 'should return the short week day name', () ->
-      equal d.strftime('%a'), 'Sun'
+      equal d.toFormat('%a'), 'Sun'
 
     it 'should return the long week day name', () ->
-      equal d.strftime('%A'), 'Sunday'
+      equal d.toFormat('%A'), 'Sunday'
 
     it 'should return the short month name', () ->
-      equal d.strftime('%b'), 'Sep'
+      equal d.toFormat('%b'), 'Sep'
 
     it 'should return the long month name', () ->
-      equal d.strftime('%B'), 'September'
+      equal d.toFormat('%B'), 'September'
 
     it 'should return the zero-padded date', () ->
-      equal d.strftime('%d'), '01'
+      equal d.toFormat('%d'), '01'
 
     it 'should return the non-padded date', () ->
-      equal d.strftime('%D'), '1'
+      equal d.toFormat('%D'), '1'
 
     it 'should return zero-padded seconds with fraction', () ->
-      equal d.strftime('%f'), '08.20'
+      equal d.toFormat('%f'), '08.20'
 
     it 'should return zero-padded hours in 24-hour format', () ->
-      equal d.strftime('%H'), '18'
+      equal d.toFormat('%H'), '18'
 
     it 'should return non-padded hours in 12-hour format', () ->
-      equal d.strftime('%i'), '6'
+      equal d.toFormat('%i'), '6'
 
     it 'should return zero-padded hours in 12-hour format', () ->
-      equal d.strftime('%I'), '06'
+      equal d.toFormat('%I'), '06'
 
     it 'should return zer-padded day of year', () ->
-      equal d.strftime('%j'), '244'
+      equal d.toFormat('%j'), '244'
 
     it 'should return zero-padded numeric month', () ->
-      equal d.strftime('%m'), '09'
+      equal d.toFormat('%m'), '09'
 
     it 'should return zero-padded minutes', () ->
-      equal d.strftime('%M'), '07'
+      equal d.toFormat('%M'), '07'
 
     it 'should return non-padded month', () ->
-      equal d.strftime('%n'), '9'
+      equal d.toFormat('%n'), '9'
 
     it 'should return non-padded minutes', () ->
-      equal d.strftime('%N'), '7'
+      equal d.toFormat('%N'), '7'
 
     it 'should return PM', () ->
-      equal d.strftime('%p'), 'p.m.'
+      equal d.toFormat('%p'), 'p.m.'
 
     it 'should return AM', () ->
       d.setHours 9
-      equal d.strftime('%p'), 'a.m.'
+      equal d.toFormat('%p'), 'a.m.'
       d.setHours 18
 
     it 'should return non-padded seconds', () ->
-      equal d.strftime('%s'), '8'
+      equal d.toFormat('%s'), '8'
 
     it 'should return padded seconds', () ->
-      equal d.strftime('%S'), '08'
+      equal d.toFormat('%S'), '08'
 
     it 'should return miliseconds', () ->
-      equal d.strftime('%r'), '200'
+      equal d.toFormat('%r'), '200'
 
     it 'should return numeric weed day', () ->
-      equal d.strftime('%w'), '0'
+      equal d.toFormat('%w'), '0'
 
     it 'should return year without century', () ->
-      equal d.strftime('%y'), '13'
+      equal d.toFormat('%y'), '13'
 
     it 'should return full year', () ->
-      equal d.strftime('%Y'), '2013'
+      equal d.toFormat('%Y'), '2013'
 
     it 'should return timezone', () ->
       # We must mock the timezone method
       originalTz = d.timezone
       d.timezone = 120
-      equal d.strftime('%z'), '+0200'
+      equal d.toFormat('%z'), '+0200'
       d.timezone = -360
-      equal d.strftime('%z'), '-0600'
+      equal d.toFormat('%z'), '-0600'
       d.timezone = originalTz
 
     it 'should return literal percent', () ->
-      equal d.strftime('%%'), '%'
+      equal d.toFormat('%%'), '%'
 
     it 'should retain non-formatting character', () ->
-      f = d.strftime('The year is %Y, around %i %p on %B %D')
+      f = d.toFormat('The year is %Y, around %i %p on %B %D')
       equal f, "The year is 2013, around 6 p.m. on September 1"
 
--- tests/test_formatting.js
+++ tests/test_formatting.js
@@ -14,94 +14,94 @@ d = new TzTime(2013, 8, 1, 18, 7, 8, 200);
 
 d1 = new TzTime(2013, 8, 1, 8, 7, 8, 200);
 
-describe('datetime.format', function() {
-  return describe('strftime()', function() {
+describe('TzTime', function() {
+  return describe('tpFormat()', function() {
     it('should return the short week day name', function() {
-      return equal(d.strftime('%a'), 'Sun');
+      return equal(d.toFormat('%a'), 'Sun');
     });
     it('should return the long week day name', function() {
-      return equal(d.strftime('%A'), 'Sunday');
+      return equal(d.toFormat('%A'), 'Sunday');
     });
     it('should return the short month name', function() {
-      return equal(d.strftime('%b'), 'Sep');
+      return equal(d.toFormat('%b'), 'Sep');
     });
     it('should return the long month name', function() {
-      return equal(d.strftime('%B'), 'September');
+      return equal(d.toFormat('%B'), 'September');
     });
     it('should return the zero-padded date', function() {
-      return equal(d.strftime('%d'), '01');
+      return equal(d.toFormat('%d'), '01');
     });
     it('should return the non-padded date', function() {
-      return equal(d.strftime('%D'), '1');
+      return equal(d.toFormat('%D'), '1');
     });
     it('should return zero-padded seconds with fraction', function() {
-      return equal(d.strftime('%f'), '08.20');
+      return equal(d.toFormat('%f'), '08.20');
     });
     it('should return zero-padded hours in 24-hour format', function() {
-      return equal(d.strftime('%H'), '18');
+      return equal(d.toFormat('%H'), '18');
     });
     it('should return non-padded hours in 12-hour format', function() {
-      return equal(d.strftime('%i'), '6');
+      return equal(d.toFormat('%i'), '6');
     });
     it('should return zero-padded hours in 12-hour format', function() {
-      return equal(d.strftime('%I'), '06');
+      return equal(d.toFormat('%I'), '06');
     });
     it('should return zer-padded day of year', function() {
-      return equal(d.strftime('%j'), '244');
+      return equal(d.toFormat('%j'), '244');
     });
     it('should return zero-padded numeric month', function() {
-      return equal(d.strftime('%m'), '09');
+      return equal(d.toFormat('%m'), '09');
     });
     it('should return zero-padded minutes', function() {
-      return equal(d.strftime('%M'), '07');
+      return equal(d.toFormat('%M'), '07');
     });
     it('should return non-padded month', function() {
-      return equal(d.strftime('%n'), '9');
+      return equal(d.toFormat('%n'), '9');
     });
     it('should return non-padded minutes', function() {
-      return equal(d.strftime('%N'), '7');
+      return equal(d.toFormat('%N'), '7');
     });
     it('should return PM', function() {
-      return equal(d.strftime('%p'), 'p.m.');
+      return equal(d.toFormat('%p'), 'p.m.');
     });
     it('should return AM', function() {
       d.setHours(9);
-      equal(d.strftime('%p'), 'a.m.');
+      equal(d.toFormat('%p'), 'a.m.');
       return d.setHours(18);
     });
     it('should return non-padded seconds', function() {
-      return equal(d.strftime('%s'), '8');
+      return equal(d.toFormat('%s'), '8');
     });
     it('should return padded seconds', function() {
-      return equal(d.strftime('%S'), '08');
+      return equal(d.toFormat('%S'), '08');
     });
     it('should return miliseconds', function() {
-      return equal(d.strftime('%r'), '200');
+      return equal(d.toFormat('%r'), '200');
     });
     it('should return numeric weed day', function() {
-      return equal(d.strftime('%w'), '0');
+      return equal(d.toFormat('%w'), '0');
     });
     it('should return year without century', function() {
-      return equal(d.strftime('%y'), '13');
+      return equal(d.toFormat('%y'), '13');
     });
     it('should return full year', function() {
-      return equal(d.strftime('%Y'), '2013');
+      return equal(d.toFormat('%Y'), '2013');
     });
     it('should return timezone', function() {
       var originalTz;
       originalTz = d.timezone;
       d.timezone = 120;
-      equal(d.strftime('%z'), '+0200');
+      equal(d.toFormat('%z'), '+0200');
       d.timezone = -360;
-      equal(d.strftime('%z'), '-0600');
+      equal(d.toFormat('%z'), '-0600');
       return d.timezone = originalTz;
     });
     it('should return literal percent', function() {
-      return equal(d.strftime('%%'), '%');
+      return equal(d.toFormat('%%'), '%');
     });
     return it('should retain non-formatting character', function() {
       var f;
-      f = d.strftime('The year is %Y, around %i %p on %B %D');
+      f = d.toFormat('The year is %Y, around %i %p on %B %D');
       return equal(f, "The year is 2013, around 6 p.m. on September 1");
     });
   });
